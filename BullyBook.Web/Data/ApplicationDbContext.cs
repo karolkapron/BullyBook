176ce6c5ad4527155b687693e594bdb5c8bd7eac
karolkapron <karolkapron11@gmail.com>
2023-09-12 12:18:21 +0200

    Add project files.

---
+++ BullyBook.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BullyBook.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BullyBook.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+     }
+}
